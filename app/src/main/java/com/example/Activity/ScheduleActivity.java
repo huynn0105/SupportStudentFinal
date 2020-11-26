8693de7140bed091e5eb5f5be390c97af5c9ac62
HuyHyun <boyhsky@gmail.com>
2020-11-26 14:07:52 +0700

    update layout schedule

--- app/src/main/java/com/example/Activity/ScheduleActivity.java
+++ app/src/main/java/com/example/Activity/ScheduleActivity.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -27,6 +28,7 @@ import com.example.quanlyhocsinh.R;
 import org.joda.time.DateTime;
 import org.joda.time.Weeks;
 import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -38,42 +40,77 @@ public class ScheduleActivity extends AppCompatActivity {
     List<ScheduleInfo> scheduleInfoList;
     RecyclerView[] rv_schedule = new RecyclerView[9];
     LinearLayout layout_schedule;
-    TextView notFound;
-    String [] week;
+    String strSemester;
     RelativeLayout[] rl = new RelativeLayout[9];
-    AutoCompleteTextView dropdown_week;
+    AutoCompleteTextView dropdown_week, dropdown_year, dropdown_semester;
     Intent intent;
     Toolbar toolbar;
     DateTime dateTime;
+    Button btn_find;
     String mssv;
     int year, semester;
+    String yearID, semesterID, weekID;
+    TextView[] date = new TextView[9];
+    TextView notFound;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
         init();
+        final String[][] weeks = new String[1][1];
         dateTime = DateTime.now();
         year = dateTime.getYear();
+        yearID = year + "";
+        dropdown_year.setText(year + "");
         semester = 0;
-        if (dateTime.getMonthOfYear() >= 8)
+        if (dateTime.getMonthOfYear() >= 8 || dateTime.getMonthOfYear()<2) {
             semester = 1;
-        else semester = 2;
+            semesterID = "1";
+            weeks[0] = getResources().getStringArray(R.array.weekOfSemster1);
 
+        } else {
+            semester = 2;
+            semesterID = "2";
+           weeks[0] = getResources().getStringArray(R.array.weekOfSemster2);
+
+        }
+        dropdown_semester.setText(semester + "");
+        String[] semester = getResources().getStringArray(R.array.semester);
+        initDropdown(dropdown_semester, semester);
+        dropdown_semester.setOnItemClickListener((adapterView, view, i, l) -> {
+            semesterID = adapterView.getAdapter().getItem(i).toString();
+            if(semesterID.equals("1"))
+                weeks[0] = getResources().getStringArray(R.array.weekOfSemster1);
+            else weeks[0] = getResources().getStringArray(R.array.weekOfSemster2);
+            dropdown_week.setText("");
+            initDropdown(dropdown_week, weeks[0]);
+        });
         getDataIntent();
-        week = getResources().getStringArray(R.array.week);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_expandable_list_item_1, week);
-        dropdown_week.setAdapter(adapter);
-        dropdown_week.setOnItemClickListener((adapterView, view, i, l) -> {
-            String wk= adapterView.getAdapter().getItem(i).toString();
-            dropdown_week.setText(wk,false);
-            setScheduleWeek(Integer.parseInt(wk));
 
+        initDropdown(dropdown_week, weeks[0]);
+        dropdown_week.setOnItemClickListener((adapterView, view, i, l) -> weekID = adapterView.getAdapter().getItem(i).toString());
+
+        String[] years = initYear(year);
+        initDropdown(dropdown_year, years);
+        dropdown_year.setOnItemClickListener((adapterView, view, i, l) -> yearID = adapterView.getAdapter().getItem(i).toString());
+
+        btn_find.setOnClickListener(view -> {
+            if (weekID.isEmpty())
+                weekID = "0";
+
+            getSchedule(mssv, Integer.parseInt(yearID), Integer.parseInt(semesterID), Integer.parseInt(weekID));
         });
 
 
+    }
 
+    private void initDropdown(AutoCompleteTextView dropdown, String[] array) {
+        final String[] txt = new String[1];
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(ScheduleActivity.this,
+                android.R.layout.simple_expandable_list_item_1, array);
+        dropdown.setAdapter(adapter);
 
     }
 
@@ -81,11 +118,8 @@ public class ScheduleActivity extends AppCompatActivity {
         intent = getIntent();
         if (intent.hasExtra("Schedule")) {
             mssv = intent.getStringExtra("Schedule");
-            if (semester != 0) {
-                getSchedule(mssv, year, semester);
-            } else {
-                Toast.makeText(this, "Có lỗi xảy ra!!!\nThử lại sau", Toast.LENGTH_SHORT).show();
-            }
+            if (semester != 0) getSchedule(mssv, year, semester, 0);
+            else Toast.makeText(this, "Có lỗi xảy ra!!!\nThử lại sau", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -97,8 +131,18 @@ public class ScheduleActivity extends AppCompatActivity {
         rv_schedule[6] = findViewById(R.id.rv_schedule6);
         rv_schedule[7] = findViewById(R.id.rv_schedule7);
         rv_schedule[8] = findViewById(R.id.rv_schedule8);
+        date[2] = findViewById(R.id.date2);
+        date[3] = findViewById(R.id.date3);
+        date[4] = findViewById(R.id.date4);
+        date[5] = findViewById(R.id.date5);
+        date[6] = findViewById(R.id.date6);
+        date[7] = findViewById(R.id.date7);
+        date[8] = findViewById(R.id.date8);
         dropdown_week = findViewById(R.id.dropdown_week);
+        dropdown_year = findViewById(R.id.dropdown_year);
+        dropdown_semester = findViewById(R.id.dropdown_semester);
         layout_schedule = findViewById(R.id.layout_schedule);
+        btn_find = findViewById(R.id.btn_find);
         notFound = findViewById(R.id.notFound);
         rl[2] = findViewById(R.id.rl2);
         rl[3] = findViewById(R.id.rl3);
@@ -118,7 +162,7 @@ public class ScheduleActivity extends AppCompatActivity {
 
     }
 
-    private void getSchedule(String studentCode, int yearID, int semester) {
+    private void getSchedule(String studentCode, int yearID, int semester, int week) {
         final ProgressDialog progressDialog = new ProgressDialog(ScheduleActivity.this);
         progressDialog.setMessage("Đang kiểm tra..");
         progressDialog.show();
@@ -137,15 +181,19 @@ public class ScheduleActivity extends AppCompatActivity {
                 if (responseEnvelope != null) {
                     scheduleInfoList = responseEnvelope.responseBody.getScheduleModel.resultSchedule;
                     if (scheduleInfoList.size() > 0) {
+                        Log.d("Dulieu",scheduleInfoList.get(0).toString());
                         layout_schedule.setVisibility(View.VISIBLE);
                         notFound.setVisibility(View.GONE);
-                        dropdown_week.setText(getWeekNum(scheduleInfoList.get(0))+"",false);
-                        setScheduleWeek(getWeekNum(scheduleInfoList.get(0)));
-
+                        if (week == 0) {
+                            dropdown_week.setText(getWeekNum(scheduleInfoList.get(0)) + "", false);
+                            weekID = getWeekNum(scheduleInfoList.get(0)) + "";
+                            setScheduleWeek(getWeekNum(scheduleInfoList.get(0)));
+                        } else
+                            setScheduleWeek(week);
 
                     } else {
-                        notFound.setVisibility(View.VISIBLE);
                         layout_schedule.setVisibility(View.GONE);
+                        notFound.setVisibility(View.VISIBLE);
                     }
                 } else {
                     Toast.makeText(ScheduleActivity.this, "Có lỗi xảy ra!!!\nThử lại sau", Toast.LENGTH_SHORT).show();
@@ -161,7 +209,8 @@ public class ScheduleActivity extends AppCompatActivity {
             }
         });
     }
-    private void setScheduleWeek(int week){
+
+    private void setScheduleWeek(int week) {
         List<ScheduleInfo>[] lists = new List[9];
         ScheduleAdapter[] adapter = new ScheduleAdapter[9];
         for (int i = 2; i <= 8; i++) {
@@ -173,13 +222,25 @@ public class ScheduleActivity extends AppCompatActivity {
                     }
                 }
             }
+            lists[i].sort((info1, info2) -> {
+                String[] time1 = info1.getTimeText().split("h");
+                String[] time2 = info2.getTimeText().split("h");
+                return Integer.parseInt(time1[0]) - Integer.parseInt(time2[0]);
+            });
             adapter[i] = new ScheduleAdapter(ScheduleActivity.this, lists[i]);
         }
-
-        for(int i =2;i<=8;i++)
+        for (int i = 2; i <= 8; i++)
             rv_schedule[i].setAdapter(adapter[i]);
 
         setViewDay(scheduleInfoList.get(0));
+    }
+
+
+    private String[] initYear(int year) {
+        String[] years = new String[6];
+        for (int i = -1; i < years.length - 1; i++)
+            years[i + 1] = String.valueOf(year - i);
+        return years;
     }
 
     @SuppressLint("SetTextI18n")
@@ -197,11 +258,16 @@ public class ScheduleActivity extends AppCompatActivity {
     @SuppressLint("SetTextI18n")
     public void setViewDay(ScheduleInfo info) {
         String dayOfWeek;
-        int day = dateTime.getDayOfWeek()+1;
-        if(dropdown_week.getText().toString().equals(getWeekNum(info)+"")){
+        int day = dateTime.getDayOfWeek() + 1;
+        if (dropdown_week.getText().toString().equals(getWeekNum(info) + "")) {
             rl[day].setBackgroundColor(Color.parseColor("#f68084"));
-        }else{
+        } else {
             rl[day].setBackgroundColor(Color.parseColor("#94bdff"));
+        }
+        DateTime dateTime1 = DateTime.parse(info.getStartDate()).plusWeeks(Integer.parseInt(dropdown_week.getText().toString()) - 1);
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/YYYY");
+        for (int i = 2; i <= 8; i++) {
+            date[i].setText(fmt.print(dateTime1.withDayOfWeek(i - 1)));
         }
     }
 
